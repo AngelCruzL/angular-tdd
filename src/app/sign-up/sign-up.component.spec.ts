8fbbde261a6cd73f2a6bf8b4644a50e146e69317
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-11 08:58:42 -0600

    Run test with testing library on karma

--- src/app/sign-up/sign-up.component.spec.ts
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -1,24 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { SignUpComponent } from './sign-up.component';
+//
+// describe('SignUpComponent', () => {
+//   let component: SignUpComponent;
+//   let fixture: ComponentFixture<SignUpComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [SignUpComponent],
+//     }).compileComponents();
+//
+//     fixture = TestBed.createComponent(SignUpComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('has sign up header', () => {
+//     const signUp = fixture.nativeElement as HTMLElement;
+//     const h1 = signUp.querySelector('h1');
+//     expect(h1?.textContent).toBe('Sign Up');
+//   });
+// });
 
+import { render, screen } from '@testing-library/angular';
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
-  let component: SignUpComponent;
-  let fixture: ComponentFixture<SignUpComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignUpComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(SignUpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('has sign up header', () => {
-    const signUp = fixture.nativeElement as HTMLElement;
-    const h1 = signUp.querySelector('h1');
-    expect(h1?.textContent).toBe('Sign Up');
+  it('has Sign Up header', async () => {
+    await render(SignUpComponent);
+    const header = screen.getByRole('heading', { name: 'Sign Up' });
+    expect(header).toBeTruthy();
   });
 });
