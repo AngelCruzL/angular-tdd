30225af95f4df2c8a0a8bb097d96d06aa6415d65
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-15 16:30:03 -0600

    Move alert and button component to components directory

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AlertComponent } from './alert/alert.component';
-import { ButtonComponent } from './button/button.component';
+import { AlertComponent } from './components/alert/alert.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [AlertComponent, ButtonComponent],
