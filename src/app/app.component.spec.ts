75894edc5311f8f592cb234f3ea0ae0d3b996c93
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-19 21:07:54 -0600

    Add and pass home and sign-up page routing karma test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { routes } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      imports: [AppModule, RouterTestingModule.withRoutes(routes)],
+      // declarations: [AppComponent, SignUpComponent, HomeComponent],
+      // imports: [
+      //   RouterTestingModule.withRoutes(routes),
+      //   HttpClientTestingModule,
+      //   SharedModule,
+      //   ReactiveFormsModule,
+      // ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    router = TestBed.inject(Router);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('passes', () => {
-    expect(true).toBe(true);
+  describe('Routing', () => {
+    const testCases = [
+      { route: '/', component: 'home', id: 'homePage' },
+      { route: '/signup', component: 'sign-up', id: 'signUpForm' },
+    ];
+
+    testCases.forEach(({ route, component, id }) => {
+      it(`should display the ${component} component on "${route}" route`, async () => {
+        await router.navigate([route]);
+        fixture.detectChanges();
+
+        const page = fixture.nativeElement.querySelector(
+          `div[data-testId="${id}"]`
+        );
+        expect(page).toBeTruthy();
+      });
+    });
   });
 });
