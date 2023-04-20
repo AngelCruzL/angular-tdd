afa2d4809254c5be28bfeff5955b07b03775f73a
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-19 21:20:24 -0600

    Add and pass home and sign-up page routing jest test cases

---
+++ src/app/app.component.jest.ts
@@ -0,0 +1,36 @@
+import { render } from '@testing-library/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from './shared/shared.module';
+import { routes } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const setup = async (route: string) => {
+  const { navigate } = await render(AppComponent, {
+    declarations: [SignUpComponent, HomeComponent],
+    imports: [HttpClientModule, SharedModule, ReactiveFormsModule],
+    routes,
+  });
+
+  await navigate(route);
+};
+
+describe('Routing', function () {
+  it.each`
+    route        | component    | id
+    ${'/'}       | ${'home'}    | ${'homePage'}
+    ${'/signup'} | ${'sign-up'} | ${'signUpForm'}
+  `(
+    'should display the $component component on "$route" route',
+    async ({ route, id }) => {
+      await setup(route);
+
+      const page = document.querySelector(`div[data-testId="${id}"]`);
+      expect(page).toBeInTheDocument();
+    }
+  );
+});
