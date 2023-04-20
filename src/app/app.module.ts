1085b38a8b9ab7338c1f143b613ce58d77172eae
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-19 20:40:28 -0600

    Add home page and app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, SignUpComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule, ReactiveFormsModule],
+  declarations: [AppComponent, SignUpComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterOutlet,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
