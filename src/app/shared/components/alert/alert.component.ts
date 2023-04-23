abbbf024e45758bdcda3d4aa1cd43890854f573a
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-23 14:58:52 -0600

    Refactor alert component to include danger and warning styles

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AlertComponent {
+  @Input() type: 'success' | 'danger' | 'info' = 'success';
 
+  get alertClass(): string {
+    const classList = ['alert', 'text-center'];
+    classList.push(`alert-${this.type}`);
+
+    return classList.join(' ');
+  }
 }
