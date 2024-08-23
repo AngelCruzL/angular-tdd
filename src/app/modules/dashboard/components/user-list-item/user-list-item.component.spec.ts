2c435efcbe0b27946937e0835f4ecee67c565738
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-08-23 09:17:39 -0600

    Add user-list-item test cases

--- src/app/modules/dashboard/components/user-list-item/user-list-item.component.spec.ts
+++ src/app/modules/dashboard/components/user-list-item/user-list-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserListItemComponent } from './user-list-item.component';
 
@@ -8,16 +9,33 @@ describe('UserListItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListItemComponent ]
-    })
-    .compileComponents();
+      declarations: [UserListItemComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserListItemComponent);
     component = fixture.componentInstance;
+    component.user = { id: 1, username: 'user1', email: 'user1@mail.com' };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display an user avatar', () => {
+    const img = fixture.nativeElement.querySelector('img');
+    expect(img).toBeTruthy();
+  });
+
+  it("should display the user's username", () => {
+    const username = fixture.nativeElement.querySelector('span');
+    expect(username.textContent).toBe('user1');
+  });
+
+  it('should have a link to user page', () => {
+    const link = fixture.nativeElement.querySelector('a');
+    expect(link).toBeTruthy();
+    expect(link.getAttribute('href')).toBe('/user/1');
   });
 });
